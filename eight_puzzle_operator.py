4822ee310515b5989d9e14dac4ed21bf95aa33a5
김예안 <yeahn@gim-yean-ui-MacBookAir.local>
2024-05-10 08:32:52 -0700

    divide files

---
+++ eight_puzzle_operator.py
@@ -0,0 +1,66 @@
+class Operator:
+    def runall(self, node):
+        left = self.move_blank_left(copy.deepcopy(node))
+        # print("left: ", left)
+        # print()
+        right = self.move_blank_right(copy.deepcopy(node))
+        # print("right: ", right)
+        # print()
+        up = self.move_blank_up(copy.deepcopy(node))
+        # print("up: ", up)
+        # print()
+        down = self.move_blank_down(copy.deepcopy(node))
+        # print("down: ", down)
+        # print()
+
+        expanded = []
+        if left != None:
+            expanded.append(left)
+        if right != None:
+            expanded.append(right)
+        if up != None:
+            expanded.append(up)
+        if down != None:
+            expanded.append(down)
+        
+        return expanded
+
+    def move_blank_left(self, node):
+        for i in range(3):
+            for j in range(3):
+                if node[i][j] == 0:
+                    if j>0:
+                        node[i][j] = node[i][j-1]
+                        node[i][j-1] = 0
+                        return node
+        return None
+
+    def move_blank_right(self, node):
+        for i in range(3):
+            for j in range(3):
+                if node[i][j] == 0:
+                    if j<2:
+                        node[i][j] = node[i][j+1]
+                        node[i][j+1] = 0
+                        return node
+        return None
+
+    def move_blank_down(self, node):
+        for i in range(3):
+            for j in range(3):
+                if node[i][j] == 0:
+                    if j<2:
+                        node[i][j] = node[i+1][j]
+                        node[i+1][j] = 0
+                        return node
+        return None
+
+    def move_blank_up(self, node):
+        for i in range(3):
+            for j in range(3):
+                if node[i][j] == 0:
+                    if i>0:
+                        node[i][j] = node[i-1][j]
+                        node[i-1][j] = 0
+                        return node
+        return None
--- uniform.py
+++ uniform.py
@@ -1,99 +1,24 @@
 import copy
 
-
-
-class Problem:
-    def __init__(self, init):
-        self.init_state = init
-    
-    def goal_test(state):
-        goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-        if state == goal:
-            return true
-        return false
-
-class Operator:
-    def runall(self, node):
-        left = self.move_blank_left(copy.deepcopy(node))
-        # print("left: ", left)
-        # print()
-        right = self.move_blank_right(copy.deepcopy(node))
-        # print("right: ", right)
-        # print()
-        up = self.move_blank_up(copy.deepcopy(node))
-        # print("up: ", up)
-        # print()
-        down = self.move_blank_down(copy.deepcopy(node))
-        # print("down: ", down)
-        # print()
-
-        expanded = []
-        if left != None:
-            expanded.append(left)
-        if right != None:
-            expanded.append(right)
-        if up != None:
-            expanded.append(up)
-        if down != None:
-            expanded.append(down)
-        
-        return expanded
-
-    def move_blank_left(self, node):
-        for i in range(3):
-            for j in range(3):
-                if node[i][j] == 0:
-                    if j>0:
-                        node[i][j] = node[i][j-1]
-                        node[i][j-1] = 0
-                        return node
-        return None
-
-    def move_blank_right(self, node):
-        for i in range(3):
-            for j in range(3):
-                if node[i][j] == 0:
-                    if j<2:
-                        node[i][j] = node[i][j+1]
-                        node[i][j+1] = 0
-                        return node
-        return None
-
-    def move_blank_down(self, node):
-        for i in range(3):
-            for j in range(3):
-                if node[i][j] == 0:
-                    if j<2:
-                        node[i][j] = node[i+1][j]
-                        node[i+1][j] = 0
-                        return node
-        return None
-
-    def move_blank_up(self, node):
-        for i in range(3):
-            for j in range(3):
-                if node[i][j] == 0:
-                    if i>0:
-                        node[i][j] = node[i-1][j]
-                        node[i-1][j] = 0
-                        return node
-        return None
+from problem import Problem
+from eight_puzzle_operator import Operator
 
 def general_search(problem, queueing_function):
-    nodes_queue = [problem.initial_state] # make queue with initial state
+    nodes_queue = [problem.init_state] # make queue with initial state
 
-    while true:
+    while True:
         if len(nodes_queue) == 0:
             return "failure"
         node = nodes_queue.pop(0)
-        if problem.goal_test(node.state):
+        if problem.goal_test(node):
             return node
         nodes_queue = queueing_function(nodes, expand(node, problem.operators))
 
 def expand(node, operators):
     return operators.runall(node)
 
-# def uniform_cost(nodes, k):
+def uniform_cost(nodes, expanded):
+    pass
 
 # l = "1 2 3"
 # n = map(lambda x: int(x), l.split())
@@ -107,12 +32,13 @@ for i in range(3):
     tmp = list(map(lambda x: int(x), tmp))
     init.append(tmp)
 
-for node in expand(init, Operator()):
-    for line in node:
-        print(line)
-    print()
+# for node in expand(init, Operator()):
+#     for line in node:
+#         print(line)
+#     print()
 
 
 
-# problem = Problem(init)
-# print(problem.init_state)
+problem = Problem(init, Operator())
+print(problem.init_state)
+# print(general_search(problem, None))
